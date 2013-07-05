7a5ca63bb2f2a52f8e750a8c4767061417802df5
Pete Muir <pmuir@bleepbleep.org.uk>
2013-07-05 16:12:00 +0100

    Add google analytics and rationalise metrics code

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -6,5 +6,6 @@ Awestruct::Extensions::Pipeline.new do
     extension Awestruct::Extensions::Indexifier.new
     helper Awestruct::Extensions::JBoss::Migration::Snippet
     helper Awestruct::Extensions::Partial
+    helper Awestruct::Extensions::GoogleAnalytics
 end
 
